6fef362cbe36bed6775f0d7d825dd87b9ca2c4f0
kookoohs <150266687+kookoohs@users.noreply.github.com>
2024-07-04 17:18:14 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,6 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< Updated upstream
-require(`dotenv`).config();
-const HOST = process.env.SERVER_HOST;
-const PORT = process.env.SERVER_PORT;
-
-const path = require(`path`);
-const fs = require(`fs`);
-
-const bodyParser = require(`body-parser`);
-App.use(bodyParser.urlencoded({ extended : true }));
-
-function Server_run() {
-    App.listen(PORT, async(req, res) => {
-        console.log(`Server runing http://${HOST}:${PORT}`);
-    })
-}
-Server_run();
-=======
 const session = require('express-session');
 app.use(session({
     secret: "암호화키",
@@ -63,4 +45,3 @@ function Server_run() {
     }
 }
 Server_run();
->>>>>>> Stashed changes
