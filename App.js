6a3ba4e288640db5edc35a9e598794836be8f614
kookoohs <150266687+kookoohs@users.noreply.github.com>
2024-07-04 17:07:35 +0900

    test

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-const Express = require(`express`);
-const App = Express();
+const express = require('express');
+const app = express();
 
+<<<<<<< Updated upstream
 require(`dotenv`).config();
 const HOST = process.env.SERVER_HOST;
 const PORT = process.env.SERVER_PORT;
@@ -17,3 +18,49 @@ function Server_run() {
     })
 }
 Server_run();
+=======
+const session = require('express-session');
+app.use(session({
+    secret: "암호화키",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+const path = require('path');
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const mysql = require('./DB');
+
+require('dotenv').config();
+const HOST = process.env.SERVER_HOST;
+const PORT = process.env.SERVER_PORT;
+
+const accountRouter = require('./routes/account');
+
+function Server_run() {
+    try {
+        mysql();
+        app.listen(PORT, async() => {
+            console.log(`Server running at http://${HOST}:${PORT}`);
+        });
+
+        app.get('/', async(req, res) => {
+            res.sendFile(path.join(__dirname, 'public', 'index.html'));
+        });
+
+        app.get('/accounts', async(req, res) => {
+            res.sendFile(path.join(__dirname, 'public', 'accounts.html'));
+        });
+
+        app.use('/api/account', accountRouter);
+    }
+    catch (error) {
+        console.error(`Server error`, error);
+    }
+}
+Server_run();
+>>>>>>> Stashed changes
