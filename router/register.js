ff0eef20dbf52d6f9cd651a7e2534499b7bba4c0
choigunwoo <gunwoo8873@outlook.kr>
2024-07-05 18:55:53 +0900

    Register Update

--- router/register.js
+++ router/register.js
@@ -1,33 +1,53 @@
-const router = require(`express`).Router();
-const { DB_connect } = require(`../DB`);
-const sha = require(`sha256`);
+const router = require('express').Router();
+const Database = require('../DB');
+const sha256 = require('sha256');
 
-router.get(`/register`, async(req, res) => {
-    res.render(`register`);
-})
-
-router.post('/register', async (req, res) => {
-    const { MySQLDB } = await DB_connect();
-    const { 
-        user_name,
-        user_email,
-        user_password,
-        user_password2,
-        user_phone,
-        user_address,
-        user_role
-    } = req.body;
-    try {
-        
-    }
+router.get('/register', (req, res) => {
+    res.sendFile('register');
 });
 
-router.get(`/login`, async(req, res) => {
-    res.render(`login`)
-})
+router.post('/save', async (req, res) => {
+    const {
+        email,
+        name,
+        password,
+        role,
+        phone_number,
+        address
+    } = req.body;
 
-router.post(`/login`, async(req, res) => {
-    res.render(`login`)
-})
+    try {
+        document.addEventListener()
+        
+        const DB = await Database();
+        const Encrypto = sha256(password + 'salt');
+        
+        const sql = `
+        INSERT INTO user (
+            email,
+            name,
+            password,
+            role,
+            phone_number,
+            address
+        ) VALUES (?, ?, ?, ?, ?, ?)`
+
+        const values = [
+            email,
+            name,
+            Encrypto,
+            role,
+            phone_number,
+            address
+        ];
+
+        const [result] = await DB.execute(sql, values);
+        res.sendFile('index');
+    }
+    catch (error) {
+        console.error('DB connect fail', error);
+        res.status(500).render('register');
+    }
+});
 
 module.exports = router;
