4cc28429d5f1235cad5f7c9e9adf58296a997029
kookoohs <150266687+kookoohs@users.noreply.github.com>
2024-07-08 10:52:38 +0900

    11

---
+++ DB.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+let db;
+const connect = async () => {
+    if (db) return db;
+
+    try {
+        db = await mysql.createConnection({
+            host: process.env.DB_HOST || 'localhost',
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME || 'app',
+        });
+
+        console.log('MySQL 연결 성공');
+        return db;
+    } catch (error) {
+        console.error('연결 오류', error);
+        throw error;
+    }
+};
+
+module.exports = connect;
